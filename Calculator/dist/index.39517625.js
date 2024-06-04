48c9015c93ac9d42d922aa988e4c2826c4a37aa6
imangadydv <148368906+imangadydv@users.noreply.github.com>
2024-06-04 12:46:54 +0530

    Add files via upload

---
+++ Calculator/dist/index.39517625.js
@@ -0,0 +1,22 @@
+const display = document.getElementById("display");
+function append(input) {
+    display.value += input;
+}
+function back() {
+    if (display.value != 0) {
+        displaylength = display.value.length;
+        remove(display.value[displaylength - 1]);
+    }
+}
+function calculate(input) {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = "error";
+    }
+}
+function clearDisplay() {
+    display.value = "";
+}
+
+//# sourceMappingURL=index.39517625.js.map
